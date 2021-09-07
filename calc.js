2d5057914f25c80aead1e54f7813199d9468c50e
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-09-07 15:07:03 -0500

    Extended calculator functionality

--- calc.js
+++ calc.js
@@ -33,28 +33,61 @@ reader.question("What would you like to calculate?", function(input){
 	if (mathSymbol === 'sqrt') {
 		console.log(Math.sqrt(num1));
 	}
+	
 
-	// Implentation of calculator using switch statement for handling mathematical operations
+	// Further study: extending calculator functionality
 
-	// switch (mathSymbol) {
-	// 	case "+":
-	// 		console.log(num1 + num2)
-	// 		break;
-	// 	case "-":
-	// 		console.log(num1 - num2)
-	// 		break;
-	// 	case "*":
-	// 		console.log(num1 * num2)
-	// 		break;
-	// 	case "/":
-	// 		console.log(num1 / num2)
-	// 		break;
-	// 	case "sqrt":
-	// 		console.log(Math.sqrt(num1))
-	// 		break;
-	// 	default:
-	// 		break;
-	// }
+	if (mathSymbol === 'sq') {
+		console.log(Math.pow(num1,2));
+	}
+
+	if (mathSymbol === 'cube') {
+		console.log(Math.pow(num1,3));
+	}
+
+	if (mathSymbol === 'pow') {
+		console.log(Math.pow(num1,num2));
+	}
+
+	if (mathSymbol === '%') {
+		console.log(num1 % num2);
+	}
+
+	//Implentation of calculator using switch statement for handling mathematical operations
+
+	switch (mathSymbol) {
+		case "+":
+			console.log(num1 + num2)
+			break;
+		case "-":
+			console.log(num1 - num2)
+			break;
+		case "*":
+			console.log(num1 * num2)
+			break;
+		case "/":
+			console.log(num1 / num2)
+			break;
+		case "sqrt":
+			console.log(Math.sqrt(num1))
+			break;
+		
+		// Further study: extending calculator functionality
+		case "sq":
+			console.log(Math.pow(num1,2));
+			break;
+		case "cube":
+			console.log(Math.pow(num1,3));
+			break;
+		case "pow":
+			console.log(Math.pow(num1,num2));
+			break;
+		case "%":
+			console.log(num1 % num2);
+			break;
+		default:
+			break;
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
