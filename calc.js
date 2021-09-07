3915201e9000a716a2e1ddc3fee611a742ab0cd5
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-09-07 14:22:28 -0500

    Implemented sum operation

--- calc.js
+++ calc.js
@@ -5,8 +5,6 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-
-
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
@@ -14,6 +12,11 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	// Implentation of calculator using if statement for handling mathematical operations
+
+	if (mathSymbol === '+') {
+		console.log(num1 + num2);
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
